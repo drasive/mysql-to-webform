f1fb656c87df2ba48eade314e2bb50f13098da96
Dimitri Vranken <dimitri.vranken@hotmail.ch>
2014-02-28 16:53:30 +0000

    Extended the interface
    git-tfs-id: [https://dimitri-vranken.visualstudio.com/DefaultCollection]$/Formulargenerator;C489

--- Formulargenerator/about.php
+++ Formulargenerator/about.php
@@ -13,24 +13,48 @@
 <body>
     <?php require('navigation.inc.html'); ?>
 
-    <!-- --------------- Content -->
-    <div class="titlebox text-center">
-        <h1>Über</h1>
-    </div>
-    <div class="container">
-        <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
-            <h2>Welcome to my fucking website!</h2>
-            <p class="lead">
-                Test. THIS IS A FUCKING TEST.
-            </p>
+    <div class="content">
+        <div class="title-box text-center">
+            <h1>Über</h1>
         </div>
-        <div class="col-xs-4 col-sm-5 col-md-4 col-lg-4 pull-right">
-            
+        <div class="container">
+            <article class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <section>
+                    <h2>Was</h2>
+                    <p>
+                        WASSSSSS
+                    </p>
+                </section>
+                <section>
+                    <h2>Warum</h2>
+                    <p>
+                        Das Erstellen eines HTML-Formular-Generators war ein Semesterbegleitender Kompetenznachsweis im Modul 307 der <a href="http://home.gibm.ch/">Gewerblich-industriellen Berufsfachschule Muttenz</a>.
+                    </p>
+                </section>
+                <section>
+                    <h2>Wer</h2>
+                    <p>
+                        Dimitri Vranken<br />
+                        Mail: <a href="mailto:dimitri.vranken@hotmail.ch">dimitri.vranken@hotmail.ch</a><br />
+                    </p>
+                </section>
+            </article>
+            <aside class="hidden-xs hidden-sm col-md-5 col-lg-4 pull-right">
+                <h2>Such free space. WOW!</h2>
+                <div class="image-box">
+                    <img src="media/images/kid_lolly_cat.jpg" alt="Ein Kleinkind, das seinen Lutscher an eine Katze geklebt hat und traurig über seinen Verlust ist. Die Katze sieht auch nicht glücklich aus." title="Ein Kleinkind, das seinen Lutscher an eine Katze geklebt hat und traurig über seinen Verlust ist. Die Katze sieht auch nicht glücklich aus." /><br />
+                    <p class="image-description">
+                        Bildquelle: <a href="http://i.imgur.com/maELdh4.jpg">Imgur.com</a><br />
+                    </p>
+                </div>
+            </aside>
         </div>
     </div>
 
+    <?php require('footer.inc.html'); ?>
+
     <!--
-        -------------------------------------------------- Scripts
+        ================================================== Scripts
     -->
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="scripts/js/bootstrap.min.js"></script>
--- Formulargenerator/form_generator.php
+++ Formulargenerator/form_generator.php
@@ -1,4 +1,4 @@
-﻿<!DOCTYPE html>
+<!DOCTYPE html>
 
 <html>
 <head>
@@ -13,59 +13,63 @@
 <body>
     <?php require('navigation.inc.html'); ?>
 
-    <div class="titlebox text-center">
-        <h1>Formular-Generator</h1>
-    </div>
-    <div class="container">
-        <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
-            <h2>Wilkommen beim Formular-Generator</h2>
-            <p class="lead">
-                Der Formular-Generator unterstützt Sie in Ihrer Aufgabe Nutzereingaben für Ihre Datenbank zu erfassen.                
-            </p>
-            <p class="lead">
-                Geben Sie einfach die erforderlichen Informationen ein und klicken Sie auf "Formular generieren".
-            </p>
-            <p class="lead">
-                Zur <a href="">Kurzanleitung</a>.
-            </p>
+    <div class="content">
+        <div class="title-box text-center">
+            <h1>Formular-Generator</h1>
         </div>
-        <div class="col-xs-4 col-sm-5 col-md-4 col-lg-4 pull-right">
-            <form method="post">
-                <div class="input-group">
-                    <label class="input-header">Formularname</label>
-                    <input type="text" id="formName" name="formName" maxlength="64" required pattern="^[^\s][a-zA-Z0-9_\- ]{1,64}$" title="Der Name des zu generierenden Formulares. Erlaubte Zeichen: a-z, A-Z, 0-9, '_', '-' und ' ' (Leerzeichen)" class="input" spellcheck="true" />
-                </div>
-                <br />
-                <div class="input-group">
-                    <label class="input-header">Hostname</label>
-                    <input type="text" id="hostname" name="hostname" maxlength="256" required pattern="^((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))$" title="Der Name des Hosts, auf dem sich die zu verwendende Datenbank befindet. Erlaubte Zeichen: Hostname oder IP-Adresse des Hosts" class="input" />
-                </div>
-                <div class="input-group">
-                    <label class="input-header">Datenbank</label>
-                    <input type="text" id="database" name="database" maxlength="256" required pattern="^\w$" title="Der Name der Datenbank, in der sich die zu verwendende Tabelle befindet. Erlaubte Zeichen: a-z, A-Z, 0-9 und '_'" class="input" />
-                </div>
-                <div class="input-group">
-                    <label class="input-header">Tabelle</label>
-                    <input type="text" id="table" name="table" maxlength="256" required pattern="^\w$" title="Der Name der Tabelle, aus der das Eingabeformular generiert werden soll. Erlaubte Zeichen: a-z, A-Z, 0-9 und '_'" class="input" />
-                </div>
-                <div class="input-group">
-                    <label class="input-header">Benutzername</label>
-                    <input type="text" id="username" name="username" maxlength="256" required pattern="^\w$" title="Der Benutzername für den Zugriff auf die angegebene Datenbank. Erlaubte Zeichen: a-z, A-Z, 0-9 und '_'" class="input" />
-                </div>
-                <div class="input-group">
-                    <label class="input-header">Passwort</label>
-                    <input type="password" id="password" name="password" maxlength="256" class="input" />
-                </div>
+        <div class="container">
+            <aside class="hidden-xs col-sm-6 col-md-6 col-lg-6">
+                <h2>Wilkommen beim Formular-Generator</h2>
+                <article>
+                    <p class="lead">
+                        Der Formular-Generator unterstützt Sie in Ihrer Aufgabe Nutzereingaben für eine Datenbank zu erfassen.                
+                    </p>
+                    <p class="lead">
+                        Geben Sie einfach die erforderlichen Informationen ein und lassen Sie ein HTML Formular generieren.
+                    </p>
+                    <p class="lead">
+                        Zur <a href="">Kurzanleitung</a>.
+                    </p>
+                </article>
+            </aside>
+            <div class="col-xs-12 col-sm-5 col-md-4 col-lg-4 pull-right">
+                <form method="post">
+                    <div class="input-group">
+                        <label class="input-header" for="formName">Formularname</label>
+                        <input type="text" id="formName" name="formName" maxlength="64" required pattern="^[^\s][a-zA-Z0-9_\- ]{1,64}$" title="Der Name des zu generierenden Formulares. Erlaubte Zeichen: a-z, A-Z, 0-9, '_', '-' und ' ' (Leerzeichen)" class="input" spellcheck="true" />
+                    </div>
+                    <br />
+                    <div class="input-group">
+                        <label class="input-header" for="hostname">Hostname</label>
+                        <input type="text" id="hostname" name="hostname" maxlength="256" required pattern="^((([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))$" title="Der Name des Hosts, auf dem sich die zu verwendende Datenbank befindet. Erlaubte Zeichen: Hostname oder IP-Adresse des Hosts" class="input" />
+                    </div>
+                    <div class="input-group">
+                        <label class="input-header" for="database">Datenbank</label>
+                        <input type="text" id="database" name="database" maxlength="256" required pattern="^\w$" title="Der Name der Datenbank, in der sich die zu verwendende Tabelle befindet. Erlaubte Zeichen: a-z, A-Z, 0-9 und '_'" class="input" />
+                    </div>
+                    <div class="input-group">
+                        <label class="input-header" for="table">Tabelle</label>
+                        <input type="text" id="table" name="table" maxlength="256" required pattern="^\w$" title="Der Name der Tabelle, aus der das Eingabeformular generiert werden soll. Erlaubte Zeichen: a-z, A-Z, 0-9 und '_'" class="input" />
+                    </div>
+                    <div class="input-group">
+                        <label class="input-header" for="username">Benutzername</label>
+                        <input type="text" id="username" name="username" maxlength="256" required pattern="^\w$" title="Der Benutzername für den Zugriff auf die angegebene Datenbank. Erlaubte Zeichen: a-z, A-Z, 0-9 und '_'" class="input" />
+                    </div>
+                    <div class="input-group">
+                        <label class="input-header" for="password">Passwort</label>
+                        <input type="password" id="password" name="password" maxlength="256" class="input" />
+                    </div>
 
-                <input type="submit" value="Formular generieren" class="submit" />
-            </form>
+                    <input type="submit" value="Formular generieren" class="submit" />
+                </form>
+            </div>
         </div>
     </div>
 
     <?php require('footer.inc.html'); ?>
 
     <!--
-        -------------------------------------------------- Scripts
+        ================================================== Scripts
     -->
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="scripts/js/bootstrap.min.js"></script>
